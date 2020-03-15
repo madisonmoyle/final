b8dbe1e8d5d2949a923d54d2940914320a8066be
madisonmoyle <madison.moyle@kellogg.northwestern.edu>
2020-03-15 23:25:32 +0000

    a meaningful commit message

--- app.rb
+++ app.rb
@@ -15,18 +15,18 @@ before { puts; puts "--------------- NEW REQUEST ---------------"; puts }       
 after { puts; }    
 
 # put your API credentials here (found on your Twilio dashboard)
-account_sid = ENV["TWILIO_ACCOUNT_SID"]
-auth_token = ENV["TWILIO_AUTH_TOKEN"]
+# account_sid = ENV["TWILIO_ACCOUNT_SID"]
+# auth_token = ENV["TWILIO_AUTH_TOKEN"]
 
-# set up a client to talk to the Twilio REST API
-client = Twilio::REST::Client.new(account_sid, auth_token)
+# # set up a client to talk to the Twilio REST API
+# client = Twilio::REST::Client.new(account_sid, auth_token)
 
-# send the SMS from your trial Twilio number to your verified non-Twilio number
-client.messages.create(
- from: "+13477897597", 
- to: "+12604093910",
- body: "Hey KIEI 451!"
-)                                                                   #
+# # send the SMS from your trial Twilio number to your verified non-Twilio number
+# client.messages.create(
+#  from: "+13477897597", 
+#  to: "+12604093910",
+#  body: "Hey KIEI 451!"
+# )                                                                   #
 #######################################################################################
 
 restaurants_table = DB.from(:restaurants)
